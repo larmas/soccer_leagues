c2721e2d81af40e65a24c497c2a206ec03d2bff0
larmas <lucas.armas.7@gmail.com>
2020-10-25 11:50:55 -0300

    API Football connection OK

--- src/app.js
+++ src/app.js
@@ -2,8 +2,42 @@ const express = require('express');
 const app = express();
 const chalk = require('chalk');
 const Database = require('./db/database');
+const axios = require('axios');
 
-database = new Database;
+const header = {'X-Auth-Token' : '43536e2a56ca4a64aaf34ed57ba1c2fb'};
+
+const apiFootball = {
+    host : 'https://api.football-data.org',
+    path : '',
+    method : 'get',
+    headers : header
+};
+
+const getRequest = (uri, apiInfo) => {
+    apiInfo.url = apiInfo.host + uri;
+    return axios(apiInfo);
+}
+
+app.get('/import-league/:cl', (req, res) => {
+    getRequest('/v2/competitions/'+req.params.cl, apiFootball)
+        .then((resp) => {
+            // Insertar liga en db
+            console.log(chalk.green.bold(resp.data.name));
+        })
+        .then((resp) => {
+            // Obtener equipos de la competicion
+            return getRequest('/v2/competitions/'+ req.params.cl + '/teams', apiFootball);
+        })
+        .then((resp) => {
+            // Insertar equipos en db
+        })
+        .then(() => {
+            // Send status code 
+        })
+        .catch((err) => {
+            console.log(chalk.red.bold(err));
+        })
+});
 
 
 app.listen(8080, () => {
