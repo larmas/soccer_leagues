ac7355fd5f6c4eb6199441802d72a62a175442c5
larmas <lucas.armas.7@gmail.com>
2020-10-25 12:54:07 -0300

    Database functionalities added

--- src/db/database.js
+++ src/db/database.js
@@ -21,6 +21,26 @@ class Database {
         });
     };
 
+    function executeQuery(query, values) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, values, (err, rows) => {
+                if (err) 
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    function endConnection(){
+        return new Promise((resolve, reject) => {
+            this.connection.end( err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            })
+        });
+    }
+
 }
 
 module.exports = Database;
